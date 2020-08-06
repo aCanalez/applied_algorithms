abf719982a0deb5aaddcd6adf33e4fc358e416cc
Alexi Canales <arubio@nexrage.com>
2020-08-05 21:36:44 -0500

    bind

---
+++ Bind.js
@@ -0,0 +1,23 @@
+//Implement Function.prototype.bind()
+
+Function.prototype.bind = function(context){
+	const fn = this;	
+	return function() {
+		fn.call(context)
+	}
+}
+
+
+function bind(fn, context){
+	return function(){
+		fn.call(context);
+	}
+}
+
+//Pass arguments
+function bind(fn, context){
+	return function(){
+		fn.apply(context, [...arguments]);
+	}
+}
+
